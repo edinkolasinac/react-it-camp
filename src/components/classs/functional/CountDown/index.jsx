707bf2177ff659e722a7bf1f3227698d45858bb0
Edo <edokolasina@hotmail.com>
2022-08-05 17:50:17 +0200

    fetch RandomCat

--- src/components/classs/functional/CountDown/index.jsx
+++ src/components/classs/functional/CountDown/index.jsx
@@ -1,6 +1,15 @@
+import { differenceInMilliseconds } from "date-fns";
+
 const CountDown = (props) => {
     const { toDate, fromDate } = props;
     const currentDate = newDate();
+
+    if (toDate) {
+        const differnce = differenceInMilliseconds(
+            currentDate,
+            newDate
+        )
+    }
     return <div>{current.Date.toDateString()}</div>
 
 
